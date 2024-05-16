790e5a3c9407a9461754bfb3f096806fc56b1118
allandonega <allan.s.donega@gmail.com>
2024-05-16 19:44:29 -0300

    Ëstrutra de Dados

--- 01 - Estrutura de dados/04 - Dicionários/00_declarando_dicionarios.py
+++ 01 - Estrutura de dados/04 - Dicionários/00_declarando_dicionarios.py
@@ -6,3 +6,7 @@ print(pessoa)
 
 pessoa["telefone"] = "3333-1234"  # {"nome": "Guilherme", "idade": 28, "telefone": "3333-1234"}
 print(pessoa)
+
+for chave in pessoa:
+    print(f"Imprime chave -> " + chave)
+    
