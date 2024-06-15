05351090fc8d5c524f4980a28e9e3630d147d25f
allandonega <allan.s.donega@gmail.com>
2024-06-15 19:56:42 -0300

    Pytz

--- 04 - Data e hora/4_pytz.py
+++ 04 - Data e hora/4_pytz.py
@@ -7,3 +7,11 @@ data2 = datetime.now(pytz.timezone("America/Sao_Paulo"))
 
 print(data)
 print(data2)
+
+############################
+# Criar ambiente virtual
+# para executar o pytz
+############################
+# python3 -m venv .env
+#  source .env/bin/activate
+#  pip install pytz
